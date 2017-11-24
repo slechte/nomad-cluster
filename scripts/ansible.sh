1d3c4f1628e38155c1411a4826b8c3bc9dd8cf09
Richard Attermeyer <richard.attermeyer@gmail.com>
2017-11-24 23:11:44 +0100

    update handling certificates

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -5,8 +5,10 @@ if hash ansible-playbook 2> /dev/null; then
         echo "ansible already installed"
 else
         echo "installing ansible"
-        grep -q -F 'deb' /etc/apt/sources.list.d/ansible.list || echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list.d/ansible.list
-        apt-add-repository -y ppa:ansible/ansible
-        apt-get update
-        apt-get install -y ansible
+        # grep -q -F 'deb' /etc/apt/sources.list.d/ansible.list || echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list.d/ansible.list
+        # apt-add-repository -y ppa:ansible/ansible
+        # apt-get update
+        # apt-get install -y ansible
+        apt-get install -y python-pip
+        pip install ansible
 fi
