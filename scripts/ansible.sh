25bd06518037c861dae4466c1e6dda7c184d6978
Richard Attermeyer <richard.attermeyer@gmail.com>
2017-11-26 10:56:34 +0100

    getting vault installation working

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -9,6 +9,9 @@ else
         # apt-add-repository -y ppa:ansible/ansible
         # apt-get update
         # apt-get install -y ansible
+        apt-get update
         apt-get install -y python-pip
+        pip install --upgrade pip
         pip install ansible
+        pip install ansible-modules-hashivault
 fi
